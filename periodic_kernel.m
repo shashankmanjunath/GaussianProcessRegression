e271e9f4ebd49a0328d47ae28d6a2462fcf881cd
jeffreyalido <31829620+jeffreyalido@users.noreply.github.com>
2021-04-06 20:11:34 -0400

    periodic kernel: faster l1 norm calculation
    replaced the nested for-loop in the periodic kernel with an efficient way to get the l1-norm

--- periodic_kernel.m
+++ periodic_kernel.m
@@ -5,11 +5,9 @@ function val = periodic_kernel(m1, m2, sigma_f, l, p, alpha)
     [m, d1] = size(m1);
     [n, d2] = size(m2);
 
-    absdist = zeros(m,n);
-    for i = 1 : m
-        for j = 1 : n
-            absdist(i,j) = norm(m1(i,:)-m2(j,:),1);
-        end
+    absdist = 0;
+    for i = 1 : d1
+        absdist = absdist + abs(m1(:,i) - m2(:,i)');
     end
     val = sigma_f^2*exp(-2 * sin(pi* absdist / p).^2 / (l^2));
 end
