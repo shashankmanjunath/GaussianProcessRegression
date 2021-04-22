3fb3d73c8ff49d00b5d88ae111adeeca13f34514
Shashank <shashankmanjunath14@gmail.com>
2021-04-22 16:34:33 -0400

    added synthetic data demo image

--- train.m
+++ train.m
@@ -1,41 +1,42 @@
 clear; close all; clc;
-addpath("kernels/")
-
+rng('default')
 rng(2021);
 
-num_train = 5;
-X_train = normrnd(0, 2.5, num_train, 1);
+num_train = 6;
+X_train = normrnd(0, 5, num_train, 1);
 y = sin(X_train);
-l = 1;
-p = 2; % for periodic kernel
-sigma_f = 1;
-noisetest = 1e-6;
-noisedata = 1e-1;
+l = 1.5;
+sigma_f = 1.0;
+noise = 1e-6;
 num_preds = 3;
-alpha = 1e-1;
-num_test = 1000;
-X_test = linspace(-5, 5, num_test)';
 
-% kernel = @(x, y)periodic_kernel(x, y, sigma_f, l, p);
-% kernel = @(x, y)rat_quad_kernel(x, y, sigma_f, l, alpha);
-kernel = @(x, y)square_exp_kernel(x, y, sigma_f, l);
-tic
-[mu_pred, var_pred] = fit_gp(X_train, y, X_test, noisetest, noisedata, kernel);
-toc
-f_post = mu_pred + var_pred * normrnd(0, 1, num_test, num_preds);
+num_test = 100;
+X_test = linspace(-10, 10, num_test)';
+
+kernel = @(x, y)square_exp_kernel(x, y, l, sigma_f);
+
+[mu_pred, var_pred] = fit_gp(X_train, y, X_test, noise, noise, kernel);
+
+f_post = mvnrnd(mu_pred, var_pred, num_preds);
 
 hold on;
 
-for i = 1:num_preds
-    plot(X_test, f_post);
-end
-scatter(X_train, y, 100, 'r', 'filled');
+plot(X_test, f_post, 'DisplayName', 'Prediction');
 
-uncertainty = 1.96 * sqrt(diag(var_pred));
+uncertainty = 2 * sqrt(diag(var_pred));
 curve1 = mu_pred - uncertainty;
 curve2 = mu_pred + uncertainty;
-plot(X_test, curve1, 'r--');
-plot(X_test, curve2, 'r--');
-plot(X_test, sin(X_test), 'k--')
-hold off;
+scatter(X_train, y, 100, 'r', 'filled', 'DisplayName', 'Training Points');
+plot(X_test, curve1, 'r--', 'DisplayName', 'Upper 95% CI Interval');
+plot(X_test, curve2, 'r--', 'DisplayName', 'Lower 95% CI Interval');
+plot(X_test, sin(X_test), 'k--', 'DisplayName', 'True Data')
 
+title("Example Gaussian Process on Synthetic Sine Data", "FontSize", 15)
+xlabel("Domain", "FontSize", 10)
+ylabel("Range", "FontSize", 10)
+
+h = gcf;
+set(h,'PaperOrientation','landscape');
+lgd = legend('Location', 'northeastoutside');
+
+hold off;
