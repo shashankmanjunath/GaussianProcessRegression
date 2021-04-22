ec4f87656dd28c7559b5375530067e9d5011cbd6
jeffreyalido <31829620+jeffreyalido@users.noreply.github.com>
2021-04-22 12:13:19 -0400

    boston results, GP polynomial kernel, and some slide stuff
    script for generating boston result figure, created new kernel, polynomial for GP, and fixed some citation stuff on GP_slides

--- train_boston.m
+++ train_boston.m
@@ -11,11 +11,27 @@ l = 0.8;
 sigma_f = 1;
 noise = 0;
 
+<<<<<<< Updated upstream
 datanoise = 0;
 p = 1;
 alpha = 1;
 kernel = @(x, y)rat_quad_kernel(x, y, sigma_f, l, p, alpha);
 [mu_pred, var_pred] = fit_gp(Xtrain, ytrain, Xtest, noise, datanoise, kernel);
+=======
+p = 2;
+c = 0;
+alpha = 1;
+
+kernel = @(x, y)square_exp_kernel(x, y, sigma_f, l);
+kernel = @(x, y)polynomial_kernel(x, y, c, p);
+% kernel = @(x, y)rat_quad_kernel(x, y, sigma_f, l, alpha);
+% kernel = @(x, y)inverse_rat_quad_kernel(x, y, sigma_f, l, alpha);
+[mu_pred_norm, var_pred_norm] = fit_gp(Xtrain_norm, ytrain_norm, Xtest_norm, noise, datanoise, kernel);
+
+% Undoing normalization
+mu_pred = (mu_pred_norm * ystd) + ymean;
+var_pred = (var_pred_norm * ystd) + ymean;
+>>>>>>> Stashed changes
 
 rmse = sqrt(mean((mu_pred - ytest).^2));
 fprintf("RMSE: %.3f\n", rmse);
